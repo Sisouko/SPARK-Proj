4f5235f237dcab4e80d8b411ab46a1c26f435342
Sisouko <ahmedabdrebbi48@gmail.com>
2026-04-30 20:46:03 +0100

    Spaark

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './style/index.css'
+import { Header } from './components/Header'
+import { ChallengeCard } from './components/Challenge-card'
+function App() {
+
+  return (
+    <main className="bg-[#1A1612] w-screen h-screen">
+    <Header/>
+    <ChallengeCard/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+return <>
+
+<div >
+    <img className = "bg-[#1A1612] w-500 h-24 p-4" src="src/assets/div.logo.svg"/>
+</div>
+
+</>
+
+}
