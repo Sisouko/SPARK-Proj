dd5d2b4f1d0048f2d7e4a6344c9e1479453334a0
Sisouko <ahmedabdrebbi48@gmail.com>
2026-05-01 13:50:17 +0100

    Spark

--- src/components/Challenge-card.jsx
+++ src/components/Challenge-card.jsx
@@ -6,7 +6,7 @@ export function ChallengeCard () {
            <div class="text-[#7A7060] text-[11px]  uppercase tracking-[0.8px] p-10">How are you feeling today?</div>
       <div className="flex gap-8 justify-center p-5">
         <div>
-        <button className="text-[#] bg-[#e8a23023] w-30 h-15 rounded"> <span class="text-[20px] block mb-1 leading-none">😴</span>Low energy </button>
+        <button className="text-[#] bg-[#] w-30 h-15 rounded"> <span class="text-[20px] block mb-1 leading-none">😴</span>Low energy </button>
       </div>
       <div>
         <button className="text-[#1A1612] bg-[#7A7060] w-30 h-15  rounded"> <span class="text-[20px] block mb-1 leading-none">😊</span>Good vibes </button>
@@ -15,11 +15,14 @@ export function ChallengeCard () {
         <button className="text-[#1A1612] bg-[#7A7060] w-30 h-15 rounded"> <span class="text-[20px] block mb-1 leading-none">🔥</span>On fire</button>
       </div>
       </div>
-      <div className="w-full h-65 ">
-        <div className="bg-[#4a8c5c2b] w-30 h-8 m-10 rounded flex items-center justify-center">
-         <h1 className="text-[#6ABF84]">Wellness</h1></div>
-<div class="text-[#7A7060] text-[11px]  uppercase tracking-[0.8px] p-10">Challenge of the day</div>
-      <div><h1 className="font text-[#F5EDD8] ">Step outside for 5 minutes</h1></div>
+      <div className="w-auto h-auto bg-[#e8a23029] flex-col items-center justify-center rounded m-10 p-1">
+        <div className="bg-[#4a8c5c80] w-30 h-8 m-10 rounded flex items-center justify-center ">
+         <h1 className="text-[#00ff4c] font-[DM]">Wellness</h1></div>
+<div class="text-[#7A7060] uppercase tracking-[0.8px] m-8">Challenge of the day</div>
+      <div><h1 className="font-[Playfair] text-[#F5EDD8] text-5xl m-8">Step outside for 5 minutes</h1>
+      <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5">Purple to Blue</button>
+
+      </div>
       </div>
       </div>
         </>
